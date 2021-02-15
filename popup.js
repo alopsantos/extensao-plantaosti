54bd09f0bb32b82b27671b343a9cf513b5b45255
alopsantos <anderson_lops@hotmail.com>
2021-02-15 18:24:00 -0300

    Feature completa

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@
   function populatePlantoes() {
     const webView = document.getElementById("webView");
     webView.innerHTML += "";
-    fetch(urlAPI + "plantoes")
+    fetch(urlAPI + "plantoes", options)
       .then((resultado) => {
         return resultado.json();
       })
@@ -11,6 +11,7 @@
         for (const plantao of plantoes) {
           webView.innerHTML += `
             <li  class="container-list">
+            <a href="#" onclick="carregarDetalhesPlantao(${plantao.id})">
             <article class="itens">
               <img src="http://app.lopscorp.com/${plantao["urllogo"]}" alt="" srcset="">
               <div class="detalhes">
@@ -23,9 +24,122 @@
                 </p>
               </div>
             </article>
+            </a>
           </li> 
             `;
         }
+      });
+  }
+  function popularPlantao() {
+    const plantaoId = "42";
+    const webView = document.getElementById("webView");
+    webView.innerHTML += "";
+    fetch(urlAPI + `plantoes/40`, options)
+      .then((resultado) => {
+        return resultado.json();
+      })
+      .then((plantao) => {
+        webView.innerHTML += `
+          <section>
+            <article class="item">
+              <img src="http://app.lopscorp.com/${plantao["urllogo"]}" alt="" srcset="">
+              <div class="detalhes">
+                <h3>${plantao.name}</h3>
+                <p>
+                  ${plantao.street}<br />
+                  ${plantao.neighborhood}<br />
+                  ${plantao.phone}<br />
+          
+                  </p>
+                  <div class="bloco">
+                  <span>Plantão</span>
+                    Inicio: ${plantao.datetimestart} | Fim: ${plantao.datetimeend}
+                    <a href="">Veja no mapa</a>
+                  </div>
+              </div>
+            </article>
+          </section>
+            `;
+      });
+  }
+  function popularDetalhesPlantao(id) {
+    const webView = document.getElementById("webView");
+    webView.innerHTML += "";
+    fetch(urlAPI + `plantoes/${id}`, options)
+      .then((resultado) => {
+        return resultado.json();
+      })
+      .then((plantao) => {
+        webView.innerHTML += `
+          <li  class="container-list">
+          
+            <article class="itens">
+              <img src="http://app.lopscorp.com/${plantao["urllogo"]}" alt="" srcset="">
+              <div class="detalhes">
+                <h3>${plantao.name}</h3>
+                <p>
+                  ${plantao.street}<br />
+                  ${plantao.neighborhood}<br />
+                </p>
+              </div>
+            </article>
+            
+          </li> 
+            `;
+      });
+  }
+  function popularFarmacias() {
+    const webView = document.getElementById("webView");
+    webView.innerHTML += "";
+    fetch(urlAPI + "farmacias", options)
+      .then((resultado) => {
+        return resultado.json();
+      })
+      .then((farmacias) => {
+        for (const farmacia of farmacias) {
+          webView.innerHTML += `
+          <li  class="container-list">
+          <a href="#" onclick="carregarDetalhesFarmacia(${farmacia.id})">
+            <article class="itens">
+              <img src="http://app.lopscorp.com/${farmacia["urllogo"]}" alt="" srcset="">
+              <div class="detalhes">
+                <h3>${farmacia.name}</h3>
+                <p>
+                  ${farmacia.street}<br />
+                  ${farmacia.neighborhood}<br />
+                </p>
+              </div>
+            </article>
+            </a>
+          </li> 
+            `;
+        }
+      });
+  }
+  function popularDetalhesFarmacia(id) {
+    const webView = document.getElementById("webView");
+    webView.innerHTML += "";
+    fetch(urlAPI + `farmacias/${id}`, options)
+      .then((resultado) => {
+        return resultado.json();
+      })
+      .then((farmacia) => {
+        webView.innerHTML += `
+          <li  class="container-list">
+          <a href="#" onclick="carregarDetalhesFarmacia(${farmacia.id})">
+            <article class="itens">
+              <img src="http://app.lopscorp.com/${farmacia["urllogo"]}" alt="" srcset="">
+              <div class="detalhes">
+                <h3>${farmacia.name}</h3>
+                <p>
+                  ${farmacia.street}<br />
+                  ${farmacia.neighborhood}<br />
+                </p>
+              </div>
+            </article>
+            </a>
+          </li> 
+            `;
       });
   }
   const options = {
@@ -42,15 +156,6 @@
       document.querySelector("menu").innerHTML = menu;
     });
 
-  fetch("./view/plantoes.html")
-    .then((response) => {
-      return response.text();
-    })
-    .then((plantoesView) => {
-      document.querySelector("content").innerHTML = plantoesView;
-      populatePlantoes();
-    });
-
   fetch("./view/footer.html")
     .then((response) => {
       return response.text();
@@ -59,9 +164,55 @@
       document.querySelector("rodape").innerHTML = rodape;
     });
 
-    const menuPlantao = document.getElementById("menu-plantao");
+  function carregarPlantao() {
+    fetch("./view/plantao.html")
+      .then((response) => {
+        return response.text();
+      })
+      .then((plantaoView) => {
+        document.querySelector("content").innerHTML = plantaoView;
+        popularPlantao();
+      });
+  }
 
-    menuPlantao.addEventListener("click", function(){
-        console.log("menu clik")
-    })
+  function carregarPlantoes() {
+    fetch("./view/plantoes.html")
+      .then((response) => {
+        return response.text();
+      })
+      .then((plantoesView) => {
+        document.querySelector("content").innerHTML = plantoesView;
+        populatePlantoes();
+      });
+  }
+  function carregarDetalhesPlantao(id) {
+    fetch("./view/detalhesPlantao.html")
+      .then((response) => {
+        return response.text();
+      })
+      .then((plantaoView) => {
+        document.querySelector("content").innerHTML = plantaoView;
+        popularDetalhesPlantao(id);
+      });
+  }
+  function carregarFarmacias() {
+    fetch("./view/farmacias.html")
+      .then((response) => {
+        return response.text();
+      })
+      .then((farmaciasView) => {
+        document.querySelector("content").innerHTML = farmaciasView;
+        popularFarmacias();
+      });
+  }
+  function carregarDetalhesFarmacia(id) {
+    fetch("./view/detalhesFarmacia.html")
+      .then((response) => {
+        return response.text();
+      })
+      .then((farmaciaView) => {
+        document.querySelector("content").innerHTML = farmaciaView;
+        popularDetalhesFarmacia(id);
+      });
+  }
 }
